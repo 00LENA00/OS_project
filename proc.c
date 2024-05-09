0f0e6a9a451ef720fc2abae858c65e512930de13
KIN0PIO <sychoi1230vv@gmail.com>
2024-05-10 01:11:27 +0900

    commit

--- proc.c
+++ proc.c
@@ -478,6 +478,29 @@ scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
+    
+        #ifdef RR
+      struct proc *p;
+      for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p->state != RUNNABLE)
+          continue;
+
+        // Switch to chosen process.  It is the process's job
+        // to release ptable.lock and then reacquire it
+        // before jumping back to us.
+        c->proc = p;
+        switchuvm(p);
+        p->num_run++;
+        p->state = RUNNING;
+
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc = 0;
+      }
+    #endif
 		#ifdef MLFQ
 
       for(int i=1; i < 5; i++)
